3b609a94845396e6057c803cc1760fd4f3f3928e
suraj chamlagain <chamlagainsuraj429@gmail.com>
2026-08-25 15:04:59 +0000

    Added Cart and CartItem APIs with automatic user and cart handling

--- orders/views.py
+++ orders/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+from rest_framework.exceptions import ValidationError
 from django.db import transaction
 from orders.models import Order, OrderItem, TrackingDetail
 from orders.serializers import OrderSerializer
-from rest_framework.response import Response
-from rest_framework import status
 from rest_framework.generics import CreateAPIView,ListAPIView,RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 from cart.models import Cart
 from decimal import Decimal
 # Create your views here.
@@ -15,22 +16,21 @@ class OrderCreateView(CreateAPIView):
     permission_classes=[IsAuthenticated]
 
     @transaction.atomic
-    def create(self,request,*args,**kwargs):
-        cart=Cart.objects.filter(user=request.user).first()
+    def perform_create(self,serializers):
+        cart=Cart.objects.filter(user=self.request.user).first()
         if not cart:
-            return Response({'message':'cart does not  exists'},status=status.HTTP_400_BAD_REQUEST)
+            raise ValidationError("Cart does not exist.")
         cart_items=cart.cart_item.all()
 
         if not cart_items.exists():
-            return Response({'message':'your cart is empty'},status=status.HTTP_400_BAD_REQUEST)
-
-        order=Order.objects.create(user=request.user,total_amount=0)
+            raise ValidationError("Cart is empty.")
+        order=Order.objects.create(user=self.request.user,total_amount=0)
 
         total_amount=Decimal('0')
         for cart_item in cart_items:
             unit_price=cart_item.product.price
             OrderItem.objects.create(order=order,product=cart_item.product,quantity=cart_item.quantity,unit_price=unit_price)
-            total_amount+=unit_price*cart_item.quantity
+            total_amount += (unit_price*cart_item.quantity)
         order.total_amount=total_amount
         order.save()
 
